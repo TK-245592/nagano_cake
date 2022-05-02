ef783b7cf475b31f19df5201ad5f7c8b65f235a1
TK-245592 <toshi4011692@gmail.com>
2022-05-02 00:27:17 +0000

    [Stat]商品詳細

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
@@ -10,14 +11,16 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to 'admin/items' 
+    redirect_to '/admin/items' 
   end 
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
+  
   
   private
   
